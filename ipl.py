795b825c438aa8f904b985e62dd905777e65a8d1
vinod-metha06 <methavinod06@gmail.com>
2021-04-06 22:15:44 +0530

    python

---
+++ ipl.py
@@ -0,0 +1,16 @@
+print("Enter your name")
+name=input()
+
+if name=="vinod":
+    print("RCB is your favourite team :)")
+elif name=="Vinod":
+    print("RCB is your favourite team :)")
+elif name=="Mayur":
+    print("RCB is your favourite team :)")
+elif name=="mayur":
+    print("RCB is your favourite team :)")
+else:
+    print("You don't have any favourite team. :(")
+    
+    
+
