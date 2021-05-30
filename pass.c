4d35b5f54a4dd743cff6972c85a43a954872b98f
vinod-metha06 <methavinod06@gmail.com>
2021-05-30 22:58:53 +0530

    last commit

--- pass.c
+++ pass.c
@@ -2,42 +2,60 @@
 
 int main(){
 
- char password[20]="1234",pass_input[20];
+ char password[]="1234",pass_input[100];
  int x = 5;
+ int flag =1;
 
     
-    printf("enter password: ");
-    gets(pass_input);
+    
     
     while (x!=0)
     {
+
+        printf("enter password: ");
+        gets(pass_input);
 
         for(int i=0;password[i]!='\0';i++)
         {
             if(password[i]==pass_input[i])
             {
-                printf("Access  Granted :) \n");
-                break;
+               
+               flag=0;
+                            
             }
+            
                 
             else
-            {
+            { flag =1;
+                printf("%d\n",x);
                 printf("Wrong Password :(\n");
+                
                 x-=1;
 
                 if(x == 0){
                     break;
 
                 }else{
+
                     printf("you have %d more attempt !! \n",x-1);
                 }
-            
+              break;
             }
-
-            
+           
+                    
         }
         
-        break;
+                
+      
+        if(flag==0)
+        {
+             printf("Access  Granted :) \n");
+             x=0;
+        }
+        
+        
     }    
+
+   
 
 }
